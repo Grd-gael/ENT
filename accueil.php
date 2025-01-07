dc0924ff02c7eb21917345250a0dd14527e7fa70
N-DaSilva <158066183+N-DaSilva@users.noreply.github.com>
2025-01-07 12:46:53 +0100

    add footer and navbar

--- accueil.php
+++ accueil.php
@@ -1,15 +1,11 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Page</title>
+    <title>ENT - Accueil</title>
 </head>
 <body>
-    <header>
-
-    </header>
-
-    <h1>Hello World</h1>
+    
 </body>
 </html>
