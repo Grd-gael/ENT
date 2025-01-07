906bf669a51fa628c6d0090f267b980939dc4538
Gaël Grandval <gaelgrandval@gmail.com>
2025-01-07 16:14:01 +0100

    nav

--- connexion.php
+++ connexion.php
@@ -3,23 +3,31 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="connexion.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Barlow:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Jost:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
     <title>ENT | Connexion</title>
 </head>
 <body>
+    <div class="container-logo">
+        <img src="img/logo-eiffel.png" alt="Logo ENT" class="logo-connexion">
+        <h1>ENT <br> Gustave Eiffel</h1>
+    </div>
     <form class="block-connexion" action="connexion.php" method="post">
-        <h1>ENT des MMI</h1>
+        <h2>Connexion</h2>
         <form action="connexion.php" method="post">
             <label for="login">Identifiant</label>
             <input type="text" name="login" placeholder="Identifiant">
-            <label for="password">Mot de Passe</label>
+            <label for="password">Mot de passe</label>
             <input type="password" name="password" id="password" placeholder="Mot de passe">
-            <input type="checkbox" name="remember" id="remember"> 
-            <label for="remember">Se souvenir de moi</label>
+            <a href="mdpoublié.php">Mot de passe oublié ?</a>
+            <div class="souvenirdemoi">
+                <input type="checkbox" name="remember" id="remember">
+                <label for="remember">Se souvenir de moi</label>
+            </div>
             <input type="submit" value="Connexion">
+            <a href="accueil.php">hhzaf</a>
     </form>
 </body>
 </html>
---
+++ navbar.php
@@ -0,0 +1,19 @@
+<nav class="navbar">
+    <div class="logo">
+        <img src="logo.png" alt="Logo" width="50">
+    </div>
+    <ul class="nav-links">
+        <li><a href="#">Services</a></li>
+        <li><a href="#">Scolarité</a></li>
+        <li><a href="#">Messagerie</a></li>
+        <li><a href="#">Espace de stockage</a></li>
+        <li><a href="#">Informations</a></li>
+        <li><a href="#">Emploi</a></li>
+        <li class="profile">
+            <a href="#">
+                <img src="img/profile.jpg" alt="Profil" class="profile-pic">
+            </a>
+        </li>
+    </ul>
+</nav>
+    
