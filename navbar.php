c714dd8e88b2abb055be633d589b39a3ebdb7fdd
Gaël Grandval <gaelgrandval@gmail.com>
2025-01-08 02:27:49 +0100

    navbar

--- navbar.php
+++ navbar.php
@@ -1,7 +1,11 @@
 <nav class="navbar">
-    <div class="logo">
-        <img src="logo.png" alt="Logo" width="50">
-    </div>
+    <a class="logo" href="#">
+        <img src="img/logo-eiffel.png" alt="Logo" width="50">
+        <div class="text-logo">
+            <span class="barlow">Espace Numérique de Travail</span> <br>
+            <span class="jost">Gustave Eiffel</span>
+        </div>
+</a>
     <ul class="nav-links">
         <li><a href="#">Services</a></li>
         <li><a href="#">Scolarité</a></li>
@@ -9,11 +13,30 @@
         <li><a href="#">Espace de stockage</a></li>
         <li><a href="#">Informations</a></li>
         <li><a href="#">Emploi</a></li>
+        <li>
+            <div class="burger">
+                <div class="line1"></div>
+                <div class="line2"></div>
+                <div class="line3"></div>
+            </div>
+        </li>
         <li class="profile">
-            <a href="#">
+            <a href="#" class="profile-link">
                 <img src="img/profile.jpg" alt="Profil" class="profile-pic">
             </a>
         </li>
     </ul>
+    
+
+    <div class="nav-active">
+        <ul>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">Scolarité</a></li>
+            <li><a href="#">Messagerie</a></li>
+            <li><a href="#">Espace de stockage</a></li>
+            <li><a href="#">Informations</a></li>
+            <li><a href="#">Emploi</a></li>
+        </ul>
+    </div>
 </nav>
     
