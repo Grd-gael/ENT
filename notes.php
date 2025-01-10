e229e76d146211d94fa6d3e7178eab29c69befe1
N-DaSilva <158066183+N-DaSilva@users.noreply.github.com>
2025-01-10 19:00:10 +0100

    add scores back

--- notes.php
+++ notes.php
@@ -23,67 +23,9 @@ require 'navbar.php';
 <h1>Notes</h1>
 
 
-<div class="container-section-fullpage">
-        <!-- Panneau des dernières notes -->
-        <div class="notes-panel">
-            <h3>Dernières notes</h3>
-            <div class="note-item" onclick="showPopup('Déploiement de service', '12/12/2024', '12/20', '2', 'Bon travail, mais peut mieux faire.')">
-                <p>Déploiement de service</p>
-                <span class="note-value">12/20</span>
-            </div>
-            <div class="note-item" onclick="showPopup('Intégration web', '14/12/2024', '17/20', '3', 'Excellent, continue ainsi !')">
-                <p>Intégration web</p>
-                <span class="note-value">17/20</span>
-            </div>
-        </div>
-
-        <!-- Tableau des matières -->
-        <div class="subjects-panel">
-            <div class="subject-card">
-                <h4>Déploiement de service</h4>
-                <div class="score-list">
-                    <span class="score" onclick="showPopup('Exercice 1', '11/12/2024', '12', '1', 'Manque de précision.')">12</span>
-                    <span class="score" onclick="showPopup('Exercice 2', '20/12/2024', '5', '1', 'Très faible.')">5</span>
-                </div>
-            </div>
-            <div class="subject-card">
-                <h4>Javascript</h4>
-                <div class="score-list">
-                    <span class="score" onclick="showPopup('Projet JS', '15/12/2024', '10', '2', 'Peut être plus optimisé.')">10</span>
-                </div>
-            </div>
-            <div class="subject-card">
-                <h4>Intégration web</h4>
-                <div class="score-list">
-                    <span class="score" onclick="showPopup('Exercice 1', '14/12/2024', '17', '1', 'Très bon travail !')">17</span>
-                </div>
-            </div>
-
-            <div class="subject-card">
-                <h4>Culture Numérique</h4>
-                <div class="score-list">
-                    <span class="score" onclick="showPopup('Exercice 1', '14/12/2024', '17', '1', 'Bon travail !')">17</span>
-                    <span class="score" onclick="showPopup('Exercice 1', '14/12/2024', '11', '1', 'À améliorer !')">11</span>
-                    <span class="score" onclick="showPopup('Exercice 1', '14/12/2024', '19', '1', 'Bravo !')">19</span>
-                    <span class="score" onclick="showPopup('Exercice 1', '20/12/2024', '16.5', '1', 'Très bon travail !')">16.5</span>
-                </div>
-            </div>
-
-        </div>
-    </div>
-
-    <!-- Popup -->
-    <div class="overlay" onclick="closePopup()"></div>
-    <div class="popup" id="popup">
-        <h4 id="popup-title">Titre du devoir</h4>
-        <p id="popup-date">Date : </p>
-        <p id="popup-note">Note : </p>
-        <p id="popup-coef">Coefficient : </p>
-        <p id="popup-comment">Commentaire : </p>
-        <button class="close-btn" onclick="closePopup()">Fermer</button>
-    </div>
-
 <?php
+require 'vue_notes.php';
+
 require 'footer.php';
 ?>
 
--- vue_notes.php
+++ vue_notes.php
@@ -1,45 +1,84 @@
 <?php
 include 'connect.php';
 
+$sql = "SELECT devoir.note, devoir.date, devoir.remarque, enonce.intitule, enonce.coeff, module.module FROM devoir JOIN enonce ON enonce.enon_id = devoir.enon_fk JOIN module ON module.modu_id = enonce.modu_fk WHERE etud_fk = :etudiant_id ORDER BY date DESC LIMIT 2;";
+$stmt = $db->prepare($sql);
+$stmt->bindParam(':etudiant_id', $_GET['id']);
+$stmt->execute();
+$last_added_notes = $stmt->fetchAll(PDO::FETCH_ASSOC);
+?>
+
+<div class="container-section-fullpage">
+    <!-- Panneau des dernières notes -->
+    <div class="notes-panel">
+    <h3>Dernières notes</h3>
+
+    <?php
+        foreach ($last_added_notes as $note) {
+            $date = date_create($note['date']);
+    ?>
+        <div class="note-item" 
+            onclick="showPopup('<?= addslashes($note['intitule']) ?>', 
+               '<?= $date -> format('d/m/Y') ?>', 
+               '<?= $note['note'] ?>/20', 
+               '<?= $note['coeff'] ?>', 
+               '<?= addslashes($note['remarque']) ?>')">
+            <p><?=$note['module']?></p>
+            <span class="note-value"><?=$note['note']?>/20</span>
+        </div>
+
+    <?php
+    }
+    ?>
+    </div>
+
+<?php
 $sql = "SELECT * FROM module;";
 $stmt = $db->query($sql);
 $modules = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-foreach ($modules as $module) {
-    echo '<div class="module">';
-    echo '<h2>' . $module['module'] . '</h2>';
+echo '<div class="subjects-panel">';
 
-    $sql = "SELECT devoir.devo_id, devoir.note, module.module FROM devoir JOIN enonce ON enonce.enon_id = devoir.enon_fk JOIN module ON module.modu_id = enonce.modu_fk WHERE devoir.etud_fk = :etudiant_id AND enonce.modu_fk = :module_id ORDER BY devoir.date DESC;";
+foreach ($modules as $module) {
+    echo '<div class="subject-card">';
+    echo "<h4>".$module['module']."</h4>";
+
+    $sql = "SELECT devoir.note, devoir.date, devoir.remarque, enonce.intitule, enonce.coeff, module.module FROM devoir JOIN enonce ON enonce.enon_id = devoir.enon_fk JOIN module ON module.modu_id = enonce.modu_fk WHERE etud_fk = :etudiant_id AND enonce.modu_fk = :module_id ORDER BY devoir.date DESC;";
     $stmt = $db->prepare($sql);
     $stmt->bindParam(':etudiant_id', $_GET['id']);
     $stmt->bindParam(':module_id', $module['modu_id']);
     $stmt->execute();
     $devoirs = $stmt->fetchAll(PDO::FETCH_ASSOC);
     
+    echo '<div class="score-list">';
+
     foreach ($devoirs as $note) {
-        echo '<a href="vue_notes.php?id=' . $_GET['id'] . '&note='.$note['devo_id'].'" class="note">'.$note['note'].'</a>';
-        echo '</div>';
+        $date = date_create($note['date']);
+        ?>
+            <span class="score" 
+    onclick="showPopup('<?= addslashes($note['intitule']) ?>', 
+                       '<?= $date -> format('d/m/Y') ?>', 
+                       '<?= $note['note'] ?>/20', 
+                       '<?= $note['coeff'] ?>', 
+                       '<?= addslashes($note['remarque']) ?>')">
+    <?=$note['note']?>
+</span>
+
+        <?php
     }
-}
-
-if (isset($_GET['note'])) {
-    $sql = "SELECT devoir.note, devoir.date, devoir.remarque, enonce.intitule, enonce.coeff, module.module, user.nom, user.prenom FROM devoir JOIN enonce ON enonce.enon_id = devoir.enon_fk JOIN module ON module.modu_id = enonce.modu_fk JOIN professeur ON professeur.prof_id = devoir.prof_fk JOIN user ON user.user_id = professeur.user_fk WHERE devoir.devo_id = :devoir_id;";
-    $stmt = $db->prepare($sql);
-    $stmt->bindParam(':devoir_id', $_GET['note']);
-    $stmt->execute();
-    $devoir = $stmt->fetch(PDO::FETCH_ASSOC);
-
-    $date = new DateTime($devoir['date']);
-
-    echo '<div class="devoir">';
-    echo '<h2>Détail de la note</h2>';
-    echo '<h3>'.$devoir['module'].'</h3>';
-    echo '<h4>'.$devoir['intitule'].'</h4>';
-    echo '<p>Ajoutée le '.$date -> format('d/m/Y').' par '.$devoir['prenom'].' '.$devoir['nom'].'</p>';
-    echo '<p>Note obtenue : '.$devoir['note'].'/20</p>';
-    echo '<p>Coeff. '.$devoir['coeff'].'</p>';
-    echo '<h4>Remarques du professeur</h4>';
-    echo '<p>'.$devoir['remarque'].'</p>';
-    echo '<a href="vue_notes.php?id='.$_GET['id'].'" class="close">Fermer</a>';
+    echo '</div>';
     echo '</div>';
 }
+echo '</div>';
+?>
+
+<!-- Popup -->
+<div class="overlay" onclick="closePopup()"></div>
+    <div class="popup" id="popup">
+        <h4 id="popup-title">Titre du devoir</h4>
+        <p id="popup-date">Date : </p>
+        <p id="popup-note">Note : </p>
+        <p id="popup-coef">Coefficient : </p>
+        <p id="popup-comment">Commentaire : </p>
+        <button class="close-btn" onclick="closePopup()">Fermer</button>
+    </div>
